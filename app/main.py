c0ec01ebed5d690e764adb8dced192ee83d295ce
rachhtord <rachael.tordoff@hotmail.co.uk>
2024-01-30 11:27:07 +0000

    added new event route

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.models import Quote
 from .routers.quote import quote_route
+from .routers.event import event_route
 
 app = FastAPI()
 
 app.include_router(quote_route, prefix="/quote")
+app.include_router(event_route, prefix="/event")
 
 
 if __name__ == "__main__":
